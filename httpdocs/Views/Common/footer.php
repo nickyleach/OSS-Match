7227cdafd18ab99cbdf3710edf7f8b084e32020d
nickyleach <nicky@nplsolutions.com>
2011-05-18 04:57:56 -0400

    Bugfix: Broken mailto link

--- httpdocs/Views/Common/footer.php
+++ httpdocs/Views/Common/footer.php
@@ -1,5 +1,5 @@
 <ul>
 	<li><a href="<?= URL::create(); ?>">Home</a></li>
 	<li><a href="<?= URL::create('Landing', 'About'); ?>">About</li>
-	<li><a href="mailto::nicky.leach@nyu.edu">Contact</a></li>
+	<li><a href="mailto:nicky.leach@nyu.edu">Contact</a></li>
 </ul>
